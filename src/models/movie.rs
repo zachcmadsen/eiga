382061da42cd9abcecc3d060b99bc09e76ce0a13
Zach Madsen <zachcmadsen@gmail.com>
2021-07-08 23:04:53 -0500

    Change every model field to an Option to match the schema

--- src/models/movie.rs
+++ src/models/movie.rs
@@ -2,86 +2,86 @@ use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Genre {
-    pub id: u64,
-    pub name: String,
+    pub id: Option<u64>,
+    pub name: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct ProductionCompany {
-    pub name: String,
-    pub id: u64,
+    pub name: Option<String>,
+    pub id: Option<u64>,
     pub logo_path: Option<String>,
-    pub origin_country: String,
+    pub origin_country: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct ProductionCountry {
-    pub iso_3166_1: String,
-    pub name: String,
+    pub iso_3166_1: Option<String>,
+    pub name: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct SpokenLanguage {
-    pub iso_639_1: String,
-    pub name: String,
+    pub iso_639_1: Option<String>,
+    pub name: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct MovieDetails {
-    pub adult: bool,
+    pub adult: Option<bool>,
     pub backdrop_path: Option<String>,
     // TODO: Add `belongs_to_collection` once we add the collection model.
     // belongs_to_collection
-    pub budget: u64,
-    pub genres: Vec<Genre>,
+    pub budget: Option<u64>,
+    pub genres: Option<Vec<Genre>>,
     pub homepage: Option<String>,
-    pub id: u64,
+    pub id: Option<u64>,
     pub imdb_id: Option<String>,
-    pub original_language: String,
-    pub original_title: String,
+    pub original_language: Option<String>,
+    pub original_title: Option<String>,
     pub overview: Option<String>,
-    pub popularity: f64,
+    pub popularity: Option<f64>,
     pub poster_path: Option<String>,
-    pub production_companies: Vec<ProductionCompany>,
-    pub production_countries: Vec<ProductionCountry>,
-    pub release_date: String,
-    pub revenue: u64,
+    pub production_companies: Option<Vec<ProductionCompany>>,
+    pub production_countries: Option<Vec<ProductionCountry>>,
+    pub release_date: Option<String>,
+    pub revenue: Option<u64>,
     pub runtime: Option<u64>,
-    pub spoken_languages: Vec<SpokenLanguage>,
+    pub spoken_languages: Option<Vec<SpokenLanguage>>,
     // TODO: Consider changing this to an enum since `status` only has six
     // allowed values.
-    pub status: String,
+    pub status: Option<String>,
     pub tagline: Option<String>,
-    pub title: String,
-    pub video: bool,
-    pub vote_average: f64,
-    pub vote_count: u64,
+    pub title: Option<String>,
+    pub video: Option<bool>,
+    pub vote_average: Option<f64>,
+    pub vote_count: Option<u64>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Title {
-    pub iso_3166_1: String,
-    pub title: String,
+    pub iso_3166_1: Option<String>,
+    pub title: Option<String>,
     #[serde(rename = "type")]
-    pub _type: String,
+    pub _type: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct AlternativeTitles {
-    pub id: u64,
-    pub titles: Vec<Title>,
+    pub id: Option<u64>,
+    pub titles: Option<Vec<Title>>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Cast {
     pub adult: Option<bool>,
-    pub gender: Option<Option<u64>>,
+    pub gender: Option<u64>,
     pub id: Option<u64>,
     pub known_for_department: Option<String>,
     pub name: Option<String>,
     pub original_name: Option<String>,
     pub popularity: Option<f64>,
-    pub profile_path: Option<Option<String>>,
+    pub profile_path: Option<String>,
     pub cast_id: Option<u64>,
     pub character: Option<String>,
     pub credit_id: Option<String>,
@@ -91,13 +91,13 @@ pub struct Cast {
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct Crew {
     pub adult: Option<bool>,
-    pub gender: Option<Option<u64>>,
+    pub gender: Option<u64>,
     pub id: Option<u64>,
     pub known_for_department: Option<String>,
     pub name: Option<String>,
     pub original_name: Option<String>,
     pub popularity: Option<f64>,
-    pub profile_path: Option<Option<String>>,
+    pub profile_path: Option<String>,
     pub credit_id: Option<String>,
     pub department: Option<String>,
     pub job: Option<String>,
