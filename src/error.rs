c16a482d4896beec4e28f074e89fda5d61b2fc88
Zach Madsen <zachcmadsen@gmail.com>
2021-12-03 18:58:21 -0600

    Handle error responses from TMDB

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,25 @@
 use std::fmt;
 
+use serde::Deserialize;
+
+/// An error from the TMDB API.
+#[derive(Debug, Deserialize)]
+pub struct TmdbError {
+    errors: Vec<String>,
+}
+
+impl fmt::Display for TmdbError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        for error in &self.errors {
+            write!(f, "{}, ", error)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl std::error::Error for TmdbError {}
+
 /// All possible errors that can occur when building endpoints and sending
 /// requests.
 #[derive(Debug)]
@@ -7,8 +27,18 @@ pub enum Error {
     InvalidHeaderValue(http::header::InvalidHeaderValue),
     Json(serde_json::Error),
     Reqwest(reqwest::Error),
-    Tmdb(String),
+    Tmdb(TmdbError),
     Url(url::ParseError),
+}
+
+impl Error {
+    /// Converts an error response from TMDB into an `Error`.
+    pub fn from_tmdb(body: &[u8]) -> Error {
+        match serde_json::from_slice::<TmdbError>(body) {
+            Ok(err) => Error::Tmdb(err),
+            Err(err) => Error::Json(err),
+        }
+    }
 }
 
 impl fmt::Display for Error {
@@ -17,7 +47,7 @@ impl fmt::Display for Error {
             Error::InvalidHeaderValue(ref err) => err.fmt(f),
             Error::Json(ref err) => err.fmt(f),
             Error::Reqwest(ref err) => err.fmt(f),
-            Error::Tmdb(ref s) => s.fmt(f),
+            Error::Tmdb(ref err) => err.fmt(f),
             Error::Url(ref err) => err.fmt(f),
         }
     }
